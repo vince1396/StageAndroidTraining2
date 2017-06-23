1f4124894ed8ca3fc430c09fa6855e2c710e3a90
Vincent <vincent.cotini96@gmail.com>
2017-06-23 12:15:05 +0200

    vinceCommit11

--- app/src/main/java/com/example/vincent/utilisateurs/MainActivity.java
+++ app/src/main/java/com/example/vincent/utilisateurs/MainActivity.java
@@ -1,22 +1,99 @@
 package com.example.vincent.utilisateurs;
 
+import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.Toast;
+import java.util.ArrayList;
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 public class MainActivity extends AppCompatActivity {
 
+    //===================================== VARIABLES GLOBALES =====================================
+    @BindView(R.id.create) Button create;
+    @BindView(R.id.valider) Button valider;
+    @BindView(R.id.name) EditText name;
+    @BindView(R.id.email) EditText email;
+    @BindView(R.id.urlimg) EditText urlimg;
+    @BindView(R.id.form) LinearLayout form;
+    Fragment listFragment = (Fragment) getSupportFragmentManager().findFragmentById(R.id.listFragment);
+    public static ArrayList<User> alRep = new ArrayList<>();
+    private boolean ifForm = false;
+    MainFragment mainFragment = new MainFragment();
 
+    //======================================== ON CREATE ===========================================
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+        mainFragment.refreshList(alRep);
+        create.setText(getString(R.string.create));
+    }
 
-        getSupportFragmentManager().beginTransaction()
-                .replace(R.id.fragmentLayout,new MainFragment())
-                .commit();
+    //===================================== CLICK SUR CREER ========================================
+    @OnClick(R.id.create)
+    public void onClickCreate() {
+
+        form.setVisibility(View.VISIBLE);
+        create.setVisibility(View.INVISIBLE);
+
+        ifForm = true;
     }
 
 
+    //===================================== CLICK SUR VALIDER ======================================
+    @OnClick(R.id.valider)
+    public void onClickValider(){
 
+        if(name.getText().toString().equals("") || email.getText().toString().equals(""))
+        {
+            Toast.makeText(this, getString(R.string.empty), Toast.LENGTH_SHORT).show();
+        }
+        else
+        {
+            String pEmail = email.getText().toString();
 
+            if (!isValidEmail((pEmail)))
+            {
+                Toast.makeText(this, getString(R.string.emailError), Toast.LENGTH_SHORT).show();
+                email.setText("");
+            }
+            else
+            {
+                form.setVisibility(View.INVISIBLE);
+                create.setVisibility(View.VISIBLE);
+                ifForm = false;
+
+                String pNom = name.getText().toString();
+                String pUrl = urlimg.getText().toString();
+
+                email.setText("");
+                name.setText("");
+                urlimg.setText("");
+
+                User user = new User();
+                user.setNom(pNom);
+                user.setEmail(pEmail);
+                user.setImg(pUrl);
+                alRep.add(user);
+            }
+        }
+    }
+
+    //===================================== VERIF EMAIL ============================================
+    public static boolean isValidEmail(CharSequence target) {
+        if(null == target) {
+            return false;
+        }
+        else {
+            return android.util.Patterns.EMAIL_ADDRESS.matcher(target).matches();
+        }
+    }
 }
--- app/src/main/java/com/example/vincent/utilisateurs/MainFragment.java
+++ app/src/main/java/com/example/vincent/utilisateurs/MainFragment.java
@@ -4,33 +4,17 @@ import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.LinearLayout;
-import android.widget.Toast;
 import java.util.ArrayList;
 import butterknife.BindView;
 import butterknife.ButterKnife;
-import butterknife.OnClick;
 
 
 public class MainFragment extends android.support.v4.app.Fragment {
 
-    @BindView(R.id.create) Button create;
-    @BindView(R.id.display) Button display;
-    @BindView(R.id.valider) Button valider;
-    @BindView(R.id.name) EditText name;
-    @BindView(R.id.email) EditText email;
-    @BindView(R.id.urlimg) EditText urlimg;
-    @BindView(R.id.form) LinearLayout form;
     @BindView(R.id.rvUser) RecyclerView myRecyclerView;
-
-    private ArrayList<User> alRep = new ArrayList<>();
-    private boolean ifForm = false;
 
     @Nullable
     @Override
@@ -38,6 +22,8 @@ public class MainFragment extends android.support.v4.app.Fragment {
 
         View view = inflater.inflate(R.layout.fragment_main, container, false);
         ButterKnife.bind(this, view);
+        myRecyclerView.setLayoutManager(new LinearLayoutManager(getActivity().getApplicationContext()));
+        myRecyclerView.setAdapter(new Adapter(MainActivity.alRep));
         return view;
     }
 
@@ -45,75 +31,10 @@ public class MainFragment extends android.support.v4.app.Fragment {
     public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
 
         super.onViewCreated(view, savedInstanceState);
-        myRecyclerView.setLayoutManager(new LinearLayoutManager(this.getActivity()));
-        myRecyclerView.setAdapter(new Adapter(alRep));
-        create.setText(getString(R.string.create));
-        display.setText(getString(R.string.display));
-        }
     }
 
-    @OnClick(R.id.create)
-    public void onClickCreate() {
+    public void refreshList(ArrayList<User> alRep){
 
-        form.setVisibility(View.VISIBLE);
-        create.setVisibility(View.INVISIBLE);
-        myRecyclerView.setVisibility(View.INVISIBLE);
-        display.setVisibility(View.INVISIBLE);
 
-        ifForm = true;
-    }
-
-    @OnClick(R.id.display)
-    public void onClickDisplay(){
-
-        myRecyclerView.setVisibility(View.VISIBLE);
-    }
-
-    public static boolean isValidEmail(CharSequence target) {
-        if(null == target) {
-            return false;
-        }
-        else {
-            return android.util.Patterns.EMAIL_ADDRESS.matcher(target).matches();
-        }
-    }
-
-    @OnClick(R.id.valider)
-    public void onClickValider(){
-
-        if(name.getText().toString().equals("") || email.getText().toString().equals(""))
-        {
-            Toast.makeText(getActivity().getBaseContext(), getString(R.string.empty), Toast.LENGTH_SHORT).show();
-        }
-        else
-        {
-            String pEmail = email.getText().toString();
-
-            if (!isValidEmail((pEmail)))
-            {
-                Toast.makeText(getActivity().getBaseContext(), getString(R.string.emailError), Toast.LENGTH_SHORT).show();
-                email.setText("");
-            }
-            else
-            {
-                form.setVisibility(View.INVISIBLE);
-                create.setVisibility(View.VISIBLE);
-                display.setVisibility(View.VISIBLE);
-                ifForm = false;
-
-                String pNom = name.getText().toString();
-                String pUrl = urlimg.getText().toString();
-
-                email.setText("");
-                name.setText("");
-                urlimg.setText("");
-
-                User user = new User();
-                user.setNom(pNom);
-                user.setEmail(pEmail);
-                user.setImg(pUrl);
-                alRep.add(user);
-            }
-        }
     }
 }
